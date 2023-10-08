c77a63db5934402d4d40bf471f73577572f2fb4d
ps-yu <pankajsharma12540@gmail.com>
2023-10-08 00:39:35 -0400

    Add navbar and backgroung image to the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import React from "react";
-import Test from "./compnents/test";
-
+import Navbar from "./compnents/navbar";
+import "./App.css"
 
 function App() {
   return(
-    <React.Fragment>
-      <p>Let's build an app</p>;
-      <Test />
-    </React.Fragment>
+    <div className="master-app">
+      <Navbar />
+    </div>
 
   ) 
 }
---
+++ src/compnents/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+  return (
+      <div className="navbar-main">
+        <div>Current City</div>
+        <div id="logo">Weather Teller </div>
+        <div>searchbox</div>
+      </div>
+  );
+}
+
+export default Navbar;
