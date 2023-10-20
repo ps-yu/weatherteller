a2c1e8818ee202203087483eb0537e5ecd8c66c2
ps-yu <pankajsharma12540@gmail.com>
2023-10-19 20:25:06 -0400

    Add animation to the app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="master-app">
       <Navbar city="toronto" />
-      <Astronomy className="astronomy.css"/>
+      <Astronomy />
     </div>
   );
 }
--- src/compnents/SearchBar.js
+++ src/compnents/SearchBar.js
@@ -8,7 +8,7 @@ const SearchBar = () => {
 
   const [location, setLocation] = useState("");
   //Toronto is the default location of the App.
-  const [endPoint, setEndPoint] = useState("toronto-ontario-canada")
+  const [endPoint, setEndPoint] = useState("toronto-ontario-canada");
 
   const url = `https://weatherapi-com.p.rapidapi.com/search.json?q=${endPoint}`;
   const options = {
@@ -17,8 +17,8 @@ const SearchBar = () => {
       "X-RapidAPI-Key": rapidapiinfo.host,
       "X-RapidAPI-Host": rapidapiinfo.key,
     },
-  }; 
-  
+  };
+
   /*
 
   
@@ -26,27 +26,24 @@ const SearchBar = () => {
   /*
   function to handle the drop down menu
   */
-  async function clickHandler(e){
-   e.preventDefault();
+  async function clickHandler(e) {
+    e.preventDefault();
     try {
       const response = await fetch(url, options);
       const result = await response.json();
-      for (let i in result){
-        if (result[i].country === 'Canada'){
+      for (let i in result) {
+        if (result[i].country === "Canada") {
           setEndPoint(result[i].url);
-        }else {
+        } else {
           setEndPoint("toronto-ontario-canada");
         }
       }
-    }
-    catch(error){
+    } catch (error) {
       console.log(error);
     }
   }
 
-  return(
-    <button onClick={clickHandler} > Search  </button>
-  )
+  return <button onClick={clickHandler}> Search </button>;
 };
 
 export default SearchBar;
